cac19b072e5d5c17ab71672fb5cc67bd9e1aa7e3
Piyapat <piyapat.thongrueng@gmail.com>
2025-05-04 17:35:41 +0700

    Display List registered users - backend

--- src/main/java/com/piyapatproject/springboot/service/UserService.java
+++ src/main/java/com/piyapatproject/springboot/service/UserService.java
@@ -3,9 +3,13 @@ package com.piyapatproject.springboot.service;
 import com.piyapatproject.springboot.dto.UserDto;
 import com.piyapatproject.springboot.entity.User;
 
+import java.util.List;
+
 public interface UserService {
 
     void saveUser(UserDto userDto);
 
     User findUserByEmail(String email);
+
+    List<UserDto>findAllUsers();
 }
