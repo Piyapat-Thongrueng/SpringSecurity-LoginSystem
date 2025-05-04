5321bafe0c7836813547c6236002c41f777398f3
Piyapat <piyapat.thongrueng@gmail.com>
2025-05-04 14:26:45 +0700

    Create Thymeleaf template for the homepage

---
+++ src/main/java/com/piyapatproject/springboot/controller/AuthController.java
@@ -0,0 +1,14 @@
+package com.piyapatproject.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    // handle method to handle home page request
+    @GetMapping("/index")
+    public String home() {
+        return "index";
+    }
+}
