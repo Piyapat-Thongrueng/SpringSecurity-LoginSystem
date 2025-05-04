0c66a1eeb8a58887388e49a80684190f8b00ce14
Piyapat <piyapat.thongrueng@gmail.com>
2025-05-04 14:44:56 +0700

    Create Handler method to handle registration form request

--- src/main/java/com/piyapatproject/springboot/controller/AuthController.java
+++ src/main/java/com/piyapatproject/springboot/controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.piyapatproject.springboot.controller;
 
+import com.piyapatproject.springboot.dto.UserDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,5 +12,15 @@ public class AuthController {
     @GetMapping("/index")
     public String home() {
         return "index";
+    }
+
+    // handle method to handle user registration form request
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        // create model object to store form data
+        UserDto user = new UserDto();
+        // add this user to a model by using this
+        model.addAttribute("user", user);
+        return "register";
     }
 }
